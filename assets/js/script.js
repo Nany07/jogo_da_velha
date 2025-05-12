3056fdd328b49f97b39b2ea882747eb557a9a3c9
Nany07 <hernanymeg@gmail.com>
2025-05-12 13:07:07 -0300

    feat(js): inclui script para tabuleiro responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
     const quadrados = document.getElementsByClassName('quadrado');
     let vencedor = document.getElementById('vencedor');
+    let simbolo_jogador= document.getElementById('jogador');
+    let button = document.getElementById('reset');
     
     let jogador = true; // true= X, false=0
 
@@ -7,13 +9,27 @@
         quadrado.addEventListener('click', function(){
            
             //jogador 1
-            if(this.innerHTML==="-"){
-                this.innerHTML= jogador? "X":"O";
-                jogador = !jogador;
+            if(this.innerHTML==="" && jogador === true){
+                this.innerHTML= "X";
+                quadrado.style.background="lightblue";
+                simbolo_jogador.innerHTML="O";
             }
+            //jogador 2 
+            if (this.innerHTML==="" && jogador === false){
+                this.innerHTML= "O";
+                simbolo_jogador.innerHTML="X";
+            }
+            jogador = !jogador;
         })
-        
     }
 
+    button.addEventListener('click',function(){
+        simbolo_jogador.innerHTML="X";
+
+        for(let quadrado of quadrados){
+            quadrado.innerHTML="";
+            quadrado.style.background="lightgrey";
+        }
+    })
 
 
