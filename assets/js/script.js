6496e28ed01fc7640fecb0549f0d0fbed2eb1ae9
Nany07 <hernanymeg@gmail.com>
2025-05-12 20:47:53 -0300

    feat(js): implementa verificacao de vencedor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,40 @@
     const quadrados = document.getElementsByClassName('quadrado');
     let vencedor = document.getElementById('vencedor');
     let simbolo_jogador= document.getElementById('jogador');
-    let button = document.getElementById('reset');
-    
+    let button = document.getElementById('reset');   
     let jogador = true; // true= X, false=0
 
+    const combinacoes_vencedoras = [
+        [0,1,2], //linha 1
+        [3,4,5], //linha 2
+        [6,7,8], // linha 3
+        [0,3,6], // coluna 1
+        [1,4,7], // coluna 2
+        [2,5,8], // coluna 3
+        [0,4,8], // diagonal \
+        [2,4,6], // diagonal /
+    ];
+
+    let tabuleiro = Array(8).fill('');
+
     for(let quadrado of quadrados){
-        quadrado.addEventListener('click', function(){
-           
+        quadrado.addEventListener('click', function(){           
             //jogador 1
-            if(this.innerHTML==="" && jogador === true){
-                this.innerHTML= "X";
+            if(quadrado.innerHTML==="" && jogador === true){
+                quadrado.innerHTML= "X";
                 quadrado.style.background="lightblue";
+                tabuleiro[quadrado.id]=true;
                 simbolo_jogador.innerHTML="O";
             }
             //jogador 2 
-            if (this.innerHTML==="" && jogador === false){
-                this.innerHTML= "O";
+            if (quadrado.innerHTML==="" && jogador === false){
+                quadrado.innerHTML= "O";
+                tabuleiro[quadrado.id]=false;
                 simbolo_jogador.innerHTML="X";
+            }
+
+            if (verificar_vitoria(jogador)){
+                vencedor.innerHTML=quadrado.innerHTML;
             }
             jogador = !jogador;
         })
@@ -33,3 +50,12 @@
     })
 
 
+    function verificar_vitoria(jogador){
+        for(let combinacao of combinacoes_vencedoras){
+            const [a,b,c]= combinacao;
+            if( tabuleiro[a]=== jogador && tabuleiro[b] === jogador  && tabuleiro[c] === jogador ){
+                return true;
+            }
+        }
+        return false;
+    }
